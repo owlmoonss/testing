cdbd97b0dcefc88f34313fd40af2806a6ceae9f3
Tachibana Shin <tachibshin@duck.com>
2024-12-06 21:43:39 +0700

    Ininital commit

---
+++ playwright.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "@playwright/test"
+
+export const baseURL = "https://magic-lpo-staging-vb6iypo3xa-an.a.run.app"
+
+export default defineConfig({
+  testDir: "tests",
+  timeout: 30000,
+  retries: 1,
+  use: {
+    browserName: "chromium",
+    headless: true,
+    baseURL
+  }
+})
