92b511467a7ceb9c2756c23c796b4cbad47e719b
owlmoonss <hello.testingcase@gmail.com>
2025-02-05 20:02:09 -0500

    Update authorization.ts

--- tests/utils/authorization.ts
+++ tests/utils/authorization.ts
@@ -6,9 +6,9 @@ export function useAuthorization() {
     await page.goto(baseURL)
     await page.getByRole("link", { name: "ログイン" }).click()
     await page.locator("#sign-in-email").click()
-    await page.locator("#sign-in-email").fill("freewebhope.vinh1@gmail.com")
+    await page.locator("#sign-in-email").fill("staging.cucua@gmail.com")
     await page.locator("#sign-in-email").press("Tab")
-    await page.locator("#sign-in-password").fill("Intern2024")
+    await page.locator("#sign-in-password").fill("fwhfwh0902")
     await page.locator("#sign-in-password").press("Enter")
 
     await page.waitForURL("**/statistics", { waitUntil: "domcontentloaded" })
